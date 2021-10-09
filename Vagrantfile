7847d31e7f08b455cdfad11d3ae641da6f13d7eb
Apolexian <ivan@niktivan.org>
2021-10-09 21:32:38 +0100

    Example client and servers for quiche now work

--- Vagrantfile
+++ Vagrantfile
@@ -1,24 +1,26 @@
 Vagrant.configure("2") do |config|
-    config.vm.box = "generic/ubuntu1804"
+    config.vm.box = "ubuntu/bionic64"
     config.ssh.forward_agent = true
     config.ssh.forward_x11 = true
+    config.ssh.insert_key = 'true'
 
     config.vm.provision "shell", inline: <<~SHELL
      add-apt-repository ppa:git-core/ppa -y
      sudo apt-get update
      sudo apt-get install -y git vim curl
      sudo apt-get install mininet -y
+     sudo PYTHON=python3 mininet/util/install.sh -n
      sudo apt-get install -y uthash-dev
      sudo apt-get install -y libev-dev
      sudo apt-get install python-pip -y
      pip install pathlib
      git clone git://github.com/mininet/mininet
      mininet/util/install.sh -fw
-     sudo apt-get install python3.7 -y
-     sudo apt-get install vim git nasm -y
+     sudo apt-get install nasm -y
      sudo apt-get install cmake libssl-dev -y
      echo 'curl https://sh.rustup.rs -sSf | sh -s -- -y;' | su vagrant
      git clone --recursive https://github.com/cloudflare/quiche
      git clone https://github.com/bschwind/mqtt-broker.git
+     git clone https://github.com/bytebeamio/rumqtt
    SHELL
   end
--- quic-test.py
+++ quic-test.py
@@ -1,55 +1,84 @@
-from mininet.topo import Topo
-from mininet.net import Mininet
-from mininet.link import TCLink
-from mininet.util import dumpNodeConnections
-from pathlib import Path
-from mininet.node import RemoteController
 import os
 import time
+from logging import error
+from threading import Thread
 
-REMOTE_CONTROLLER_IP = "127.0.0.1"
+from mininet.cli import CLI
+from mininet.link import TCLink
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.topo import Topo
+
+"""
+This file provides a script to run the example client and server of quiche.
+
+It uses a simple topology with 3 clients and one server. Clients connect to
+the server in parallel.
+
+Due to how quiche works this script has to be run from the quiche/examples/
+directory.
+
+The source code for quiche example client and server can be found at:
+https://github.com/cloudflare/quiche/tree/master/examples
+"""
 
 
 class SingleSwitchTopo(Topo):
     def __init__(self, n=2, **opts):
         Topo.__init__(self, **opts)
-        switch = self.addSwitch('s1')
+        switch = self.addSwitch('switch1')
         for h in range(n):
             host = self.addHost('h%s' % (h + 1))
             self.addLink(host, switch, bw=10)
 
 
 def run():
-    topo = SingleSwitchTopo()
-    net = Mininet(topo=topo, link=TCLink,
-                  controller=None,
-                  autoStaticArp=True)
-    net.addController("c0",
-                      controller=RemoteController,
-                      ip=REMOTE_CONTROLLER_IP,
-                      port=6633)
+    num_clients = 4
+    topo = SingleSwitchTopo(n=4)
+    net = Mininet(topo=topo, host=CPULimitedHost,
+                  link=TCLink, autoPinCpus=True)
+
     net.start()
+    CLI(net)
     time.sleep(1)
-    dumpNodeConnections(net.hosts)
+
+    clients = list()
+    for i in range(num_clients - 1):
+        client = net.get('h{idx}'.format(idx=i + 1))
+        clients.append(client)
+
+    server = net.get('h4')
+
     if not os.path.exists('data'):
         os.makedirs('data')
-        Path('data/quic-client').touch()
-        Path('data/quic-server').touch()
-        Path('data/quic-ping').touch()
 
-    client = net.get('h1')
-    server = net.get('h2')
+    server.cmd('sudo ./server ',
+               server.IP(), ' 443 &> data/quic-server &')
 
-    client.cmd('ping ', server.IP(), '5006 &> data/quic-ping &')
+    threads = list()
+    for i in range(num_clients - 1):
+        try:
+            x = Thread(target=client_send, args=(clients[i], server, i + 1))
+            x.daemon = True
+            threads.append(x)
+            x.start()
+        except error:
+            print(error)
 
-    server.cmd('./quiche/examples/server ',
-               server.IP(), ' 5006 &> data/quic-server &')
-    for _ in range(5):
-        client.cmd('./quiche/examples/client ',
-                   server.IP(), ' 5006 &> data/quic-client &')
-        time.sleep(1)
+    for thread in threads:
+        thread.join()
 
     net.stop()
+
+
+def client_send(client, server, num):
+    print(client)
+    for i in range(5):
+        client.cmd('sudo ./client ',
+                   server.IP(), ' 443 &> data/quic-client-{num}-{idx} &'.format(num=num, idx=i))
+        time.sleep(5)
+        print(
+            "*** Client {client_idx} done with iteration {idx}".format(client_idx=num, idx=i))
 
 
 if __name__ == '__main__':
