845c1e0716707957ac28d8ec79f4414082973474
Apolexian <ivan@niktivan.org>
2021-10-27 00:40:41 +0100

    minimal reproducable test env

--- socket/client/src/main.rs
+++ socket/client/src/main.rs
@@ -1,7 +1,13 @@
-use quic_socket;
+use quic_socket::QuicSocket;
+use tokio;
 
-fn main() {
-    let addr = "127.0.0.1:4444".parse().unwrap();
-    let socket = QuickSocket::bind(addr);
-    let listener = socket.connect()?;
+#[tokio::main]
+async fn main() {
+    let addr = "0.0.0.0:4443".parse().unwrap();
+    let socket = QuicSocket::bind(addr).await.unwrap();
+    let acc_addr = "0.0.0.0:4444".parse().unwrap();
+    let mut out = [0; 1350];
+    let mut listener = socket.accept(acc_addr).unwrap();
+    let write = listener.send(&mut out).await.expect("initial send failed");
+    println!("{:?}", &out[..write])
 }
--- socket/server/src/main.rs
+++ socket/server/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use quic_socket::QuicSocket;
+use tokio;
+
+#[tokio::main]
+async fn main() {
+    let addr = "0.0.0.0:4444".parse().unwrap();
+    let socket = QuicSocket::bind(addr).await.unwrap();
+    let con_addr = "0.0.0.0:4443".parse().unwrap();
+    let mut listener = socket.connect(con_addr).unwrap();
+    let mut buf = [0; 65535];
+    loop {
+        let read = match listener.recv(&mut buf).await {
+            Ok(r) => r,
+            Err(e) => {
+                println!("{:?}", e);
+                return;
+            }
+        };
+        println!("{:?}", &buf[..read])
+    }
 }
