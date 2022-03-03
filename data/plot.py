a02a952c05443e34a247460dc71cfb2774a35060
Apolexian <ivan@niktivan.org>
2022-03-03 00:43:44 +0000

    plots

--- data/plot.py
+++ data/plot.py
@@ -4,6 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from math import pi
 
+
 def show_values(axs, orient="v", space=.01):
     def _single(ax):
         if orient == "v":
@@ -11,7 +12,7 @@ def show_values(axs, orient="v", space=.01):
                 _x = p.get_x() + p.get_width() / 2
                 _y = p.get_y() + p.get_height() + (p.get_height()*0.01)
                 value = '{:.1f}'.format(p.get_height())
-                ax.text(_x, _y, value, ha="center") 
+                ax.text(_x, _y, value, ha="center")
         elif orient == "h":
             for p in ax.patches:
                 _x = p.get_x() + p.get_width() + float(space)
@@ -25,8 +26,11 @@ def show_values(axs, orient="v", space=.01):
     else:
         _single(axs)
 
-impls = ["ngtcp2", "ngtcp2", "picoquic", "picoquic", "msquic","msquic", "quic-go", "quic-go", "Quinn", "Quinn", "Quiche","Quiche", "mvfst", "mvfst"]
-footprint = [3.4, 4.3, 3.2, 3.9, 3.2, 4.1, 8.7, 9.9, 9.1, 9.5, 7.8, 7.0, 11.1, 12.0]
+
+impls = ["ngtcp2", "ngtcp2", "picoquic", "picoquic", "msquic", "msquic",
+         "quic-go", "quic-go", "Quinn", "Quinn", "Quiche", "Quiche", "mvfst", "mvfst"]
+footprint = [3.4, 4.3, 3.2, 3.9, 3.2, 4.1,
+             8.7, 9.9, 9.1, 9.5, 7.8, 7.0, 11.1, 12.0]
 tag = ["Client", "Server"] * 7
 
 data = list()
@@ -47,11 +51,12 @@ plt.xlabel('Implementation')
 plt.ylabel('Size (MiB)')
 
 sns.despine()
-plt.savefig("quic_impls.png",dpi=500)
+plt.savefig("quic_impls.png", dpi=500)
 
 plt.clf()
 
-components = ["std", "quinn", "regex", "rustls", "tokio", "ring", "url", "idna", "other"]
+components = ["std", "quinn", "regex", "rustls",
+              "tokio", "ring", "url", "idna", "other"]
 percents = [23, 17.8, 14.3, 12.6, 5.9, 5.8, 1.8, 1.5, 17.3]
 data = list()
 data.append(components)
@@ -66,12 +71,13 @@ plt.ylabel('Contribution (%)')
 plt.title("Crates by largest contribution to the client's binary size")
 sns.despine()
 show_values(ax)
-plt.savefig("mquictt_binary_client.png",dpi=500)
+plt.savefig("mquictt_binary_client.png", dpi=500)
 
 
 plt.clf()
 
-components = ["std", "quinn", "rustls", "regex", "h2", "tokio", "hyper", "aho\n_corasick", "other"]
+components = ["std", "quinn", "rustls", "regex",
+              "h2", "tokio", "hyper", "aho\n_corasick", "other"]
 percents = [16.9, 16.6, 16.3, 8.8, 7.3, 3.2, 3.0, 1.9, 26]
 data = list()
 data.append(components)
@@ -86,7 +92,7 @@ plt.ylabel('Contribution (%)')
 plt.title("Crates by largest contribution to the broker's binary size")
 sns.despine()
 show_values(ax)
-plt.savefig("mquictt_binary_broker.png",dpi=500)
+plt.savefig("mquictt_binary_broker.png", dpi=500)
 
 
 plt.clf()
@@ -106,7 +112,7 @@ plt.ylabel('Binary Size (KiB)')
 plt.title("Reduction in broker's binary size based on stage of reduction")
 sns.despine()
 show_values(ax)
-plt.savefig("quinn_binary_reduce_broker.png",dpi=500)
+plt.savefig("quinn_binary_reduce_broker.png", dpi=500)
 
 plt.clf()
 
@@ -125,48 +131,40 @@ plt.ylabel('Binary Size (KiB)')
 plt.title("Reduction in client's binary size based on stage of reduction")
 sns.despine()
 show_values(ax)
-plt.savefig("quinn_binary_reduce_client.png",dpi=500)
+plt.savefig("quinn_binary_reduce_client.png", dpi=500)
 
 plt.clf()
 
-df_mqtt_tcp = pd.read_csv("mqtt/rumqtt_base_filtered/results_connection_time.csv")
-categories=set(df_mqtt_tcp["link"])
-N = len(categories)
-angles = [n / float(N) * 2 * pi for n in range(N)]
-angles += angles[:1]
-plt.figure(figsize=(8,8)) 
-ax = plt.subplot(111, polar=True)
-ax.set_theta_offset(pi / 2)
-ax.set_theta_direction(-1)
-plt.xticks(angles[:-1], categories)
-ax.set_rlabel_position(0)
-plt.yticks([0,0.001,0.02], ["0","0.01","0.02"], color="grey", size=7)
-plt.ylim(0,0.03)
-values_home = df_mqtt_tcp.loc[df_mqtt_tcp['scenario'] == "home"]
-values_farm = df_mqtt_tcp.loc[df_mqtt_tcp['scenario'] == "farm"]
-values_synth = df_mqtt_tcp.loc[df_mqtt_tcp['scenario'] == "synth"]
+df_mqtt_tcp = pd.read_csv(
+    "mqtt/results_connection_time.csv")
+g = sns.FacetGrid(df_mqtt_tcp, col="scenario", row="link", hue="type",
+                  margin_titles=True, height=4, aspect=1.33, palette="Set2")
+g.map(sns.barplot, 'type', 'time', order=df_mqtt_tcp.type.unique()).add_legend()
+g.set_axis_labels("Implementation", "Time (ms)",)
+for ax in g.axes.flatten():
+    for p in ax.patches:
+        ax.annotate("%.3f" % p.get_height(), (p.get_x() + p.get_width() / 2., p.get_height()),
+                    ha='center', va='center', fontsize=11, color='black', xytext=(0, 5),
+                    textcoords='offset points')
+g.fig.subplots_adjust(top=0.9)
+g.fig.suptitle(
+    "Time for transport protocol\nto establish connection in base rumqtt vs MQuicTTquic.\n")
+plt.savefig("analysis_connection_time.png", dpi=500)
 
-h = []
-f = []
-s = []
+plt.clf()
 
-for c in categories:
-    h.append(values_home.loc[values_home['link'] == c]['time'].values.tolist()[0])
-    f.append(values_farm.loc[values_farm['link'] == c]['time'].values.tolist()[0])
-    s.append(values_synth.loc[values_synth['link'] == c]['time'].values.tolist()[0])
-
-h += h[:1]
-f += f[:1]
-s += s[:1]
-
-print(h)
-print(angles)
-ax.plot(angles, h, linewidth=1, linestyle='solid', label="Home")
-ax.fill(angles, h, 'b', alpha=0.1)
-ax.plot(angles, f, linewidth=1, linestyle='solid', label="Farm")
-ax.fill(angles, f, 'r', alpha=0.1)
-ax.plot(angles, s, linewidth=1, linestyle='solid', label="Synth")
-ax.fill(angles, s, 'g', alpha=0.1)
-plt.legend(loc='upper right', bbox_to_anchor=(0.05, 0.05))
-plt.title("Time for transport protocol\nto establish connection in base rumqtt.\n")
-plt.savefig("rumqtt_base_connection_time.png",dpi=500)
+df_mqtt_tcp = pd.read_csv(
+    "mqtt/results_comm_time.csv")
+g = sns.FacetGrid(df_mqtt_tcp, col="scenario", row="link", hue="type",
+                  margin_titles=True, height=4, aspect=1.33, palette="Set2")
+g.map(sns.barplot, 'type', 'time', order=df_mqtt_tcp.type.unique()).add_legend()
+g.set_axis_labels("Implementation", "Time (s)",)
+for ax in g.axes.flatten():
+    for p in ax.patches:
+        ax.annotate("%.3f" % p.get_height(), (p.get_x() + p.get_width() / 2., p.get_height()),
+                    ha='center', va='center', fontsize=11, color='black', xytext=(0, 5),
+                    textcoords='offset points')
+g.fig.subplots_adjust(top=0.9)
+g.fig.suptitle(
+    "Time taken to transmit all data in base rumqtt vs MQuicTT.\n")
+plt.savefig("analysis_comm_time.png", dpi=500)
